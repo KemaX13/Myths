3263bd13aaf82b95b11f5f465537dc74c0de6267
Genta Mateo Kamekawa Roa <73597287+KemaX13@users.noreply.github.com>
2021-05-26 18:16:43 +0200

    va funcionar, diria q no

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2904f960fd27ca771af91ff97502fe3bcc121714',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2904f960fd27ca771af91ff97502fe3bcc121714',
+    ),
+    'heroku/heroku-buildpack-php' => 
+    array (
+      'pretty_version' => 'v192',
+      'version' => '192.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '360fa3ee3caad234760048da8bc8fefd35609ea9',
+    ),
+  ),
+);
